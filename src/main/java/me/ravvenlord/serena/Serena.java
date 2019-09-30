0ea3ff6137f73f9ae9136808beb673a0d203e3f6
Bjarne Koll <LynxPlay101@gmail.com>
2019-09-30 15:39:15 +0200

    Rename main entrypoint command to hoggyback
    This commit changes the basic name of the serena command that is used to
    toggle serena functionality to 'hoggyback'.

--- src/main/java/me/ravvenlord/serena/Serena.java
+++ src/main/java/me/ravvenlord/serena/Serena.java
@@ -111,7 +111,7 @@ public class Serena extends JavaPlugin implements Reloadable {
             , LanguageConfiguration languageConfiguration
             , PlayerPermissionChecker permissionChecker) {
 
-        Objects.requireNonNull(getCommand("serena")).setExecutor(new SerenaToggleCommand(playerToggleRegistry
+        Objects.requireNonNull(getCommand("hoggyback")).setExecutor(new SerenaToggleCommand(playerToggleRegistry
                 , languageConfiguration
                 , permissionChecker));
         Objects.requireNonNull(getCommand("serena-reload")).setExecutor(new SerenaReloadCommand(permissionChecker
